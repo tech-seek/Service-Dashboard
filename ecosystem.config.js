1a395531cadd7572aec3272c4bdf9d6dd231983a
rakib-siddiki <rmrakib901@gmail.com>
2025-03-21 15:42:20 +0000

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,20 @@
-module.exports = {
-    apps : [
-        {
-            name: "netflix-app",
-            script: "npm start",
-            port: 3000
-        }
-    ]
-}
+ module.exports = {
+  apps: [
+    {
+      name: "netflix-app",
+      script: "npm", // Change this to "npm"
+      args: "start", // Add this line
+      instances: 1, // Or 'max' for clustering
+      autorestart: true,
+      watch: true,
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000 // Set the PORT environment variable
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000 // Set the PORT environment variable
+      }
+    }
+  ]
+};
